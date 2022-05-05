08206333efe281fe6f4631bff86e3c88c0c0e1eb
Ferşat <58539842+fers4t@users.noreply.github.com>
2022-05-05 04:13:37 +0300

    [refactor]: removed unnecessary files from project

--- dist/index.modern.js
+++ dist/index.modern.js
@@ -1,38 +1,81 @@
 import React, { useRef, useState, useCallback, useEffect } from 'react';
 
+function _extends() {
+  _extends = Object.assign || function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
+    }
+
+    return target;
+  };
+
+  return _extends.apply(this, arguments);
+}
+
+function _objectWithoutPropertiesLoose(source, excluded) {
+  if (source == null) return {};
+  var target = {};
+  var sourceKeys = Object.keys(source);
+  var key, i;
+
+  for (i = 0; i < sourceKeys.length; i++) {
+    key = sourceKeys[i];
+    if (excluded.indexOf(key) >= 0) continue;
+    target[key] = source[key];
+  }
+
+  return target;
+}
+
+var _excluded = ["maxColumnCount", "minColumnCount", "children", "gap"],
+    _excluded2 = ["minWidth", "children"];
 function GridAutoResponsiveWrapper(props) {
-  const {
-    maxColumnCount,
-    minColumnCount = 1,
-    gap = 0,
-    children
-  } = props;
-  const wrapperRef = useRef(null);
-  const [wrapperWidth, setWrapperWidth] = useState();
-  const gridItemWidthInWrapper = Array.from(children)[0].props.minWidth;
-  const [currentRowCount, setCurrentRowCount] = useState();
-  const handleWindowResize = useCallback(() => {
+  var maxColumnCount = props.maxColumnCount,
+      _props$minColumnCount = props.minColumnCount,
+      minColumnCount = _props$minColumnCount === void 0 ? 1 : _props$minColumnCount,
+      _props$gap = props.gap,
+      gap = _props$gap === void 0 ? 0 : _props$gap,
+      children = props.children;
+  var wrapperRef = useRef(null);
+
+  var _useState = useState(),
+      wrapperWidth = _useState[0],
+      setWrapperWidth = _useState[1];
+
+  var gridItemWidthInWrapper = Array.from(children)[0].props.minWidth;
+
+  var _useState2 = useState(),
+      currentRowCount = _useState2[0],
+      setCurrentRowCount = _useState2[1];
+
+  var handleWindowResize = useCallback(function () {
     var _wrapperRef$current;
 
     setWrapperWidth((_wrapperRef$current = wrapperRef.current) === null || _wrapperRef$current === void 0 ? void 0 : _wrapperRef$current.offsetWidth);
   }, []);
-  useEffect(() => {
+  useEffect(function () {
     if (wrapperRef.current) {
       setWrapperWidth(wrapperRef.current.clientWidth);
     }
   }, [wrapperRef]);
-  useEffect(() => {
+  useEffect(function () {
     window.addEventListener('resize', handleWindowResize);
-    return () => {
+    return function () {
       window.removeEventListener('resize', handleWindowResize);
     };
   }, [handleWindowResize]);
-  useEffect(() => {
+  useEffect(function () {
     if (wrapperWidth && gridItemWidthInWrapper && gap && maxColumnCount) {
       if (wrapperWidth - gap * (maxColumnCount - 1) >= gridItemWidthInWrapper * maxColumnCount) {
         setCurrentRowCount(maxColumnCount);
       } else {
-        const autoResponsiveCols = Math.floor((wrapperWidth - gap * (maxColumnCount - 1)) / gridItemWidthInWrapper);
+        var autoResponsiveCols = Math.floor((wrapperWidth - gap * (maxColumnCount - 1)) / gridItemWidthInWrapper);
 
         if (autoResponsiveCols <= minColumnCount) {
           setCurrentRowCount(minColumnCount);
@@ -43,43 +86,38 @@ function GridAutoResponsiveWrapper(props) {
     }
   }, [wrapperWidth, gridItemWidthInWrapper]);
 
-  const jsxAttributes = (({
-    maxColumnCount,
-    minColumnCount,
-    children,
-    gap,
-    ...o
-  }) => o)(props);
+  var jsxAttributes = function (_ref) {
+    var o = _objectWithoutPropertiesLoose(_ref, _excluded);
+
+    return o;
+  }(props);
 
   return React.createElement("div", Object.assign({
     ref: wrapperRef,
     style: {
       display: 'grid',
-      gridTemplateColumns: `repeat(${currentRowCount}, 1fr)`,
+      gridTemplateColumns: "repeat(" + currentRowCount + ", 1fr)",
       gridGap: gap + 'px'
     }
   }, jsxAttributes), children);
 }
 function GridAutoResponsiveItem(props) {
-  const {
-    children,
-    minWidth
-  } = props;
-  const child = React.Children.only(children);
+  var children = props.children,
+      minWidth = props.minWidth;
+  var child = React.Children.only(children);
 
-  const jsxAttributes = (({
-    minWidth,
-    children,
-    ...o
-  }) => o)(props);
+  var jsxAttributes = function (_ref2) {
+    var o = _objectWithoutPropertiesLoose(_ref2, _excluded2);
 
-  const singleGridElementWithMinWidth = React.cloneElement(child, {
+    return o;
+  }(props);
+
+  var singleGridElementWithMinWidth = React.cloneElement(child, _extends({
     style: {
       minWidth: minWidth + 'px',
       width: '100%'
-    },
-    ...jsxAttributes
-  });
+    }
+  }, jsxAttributes));
   return singleGridElementWithMinWidth;
 }
 
